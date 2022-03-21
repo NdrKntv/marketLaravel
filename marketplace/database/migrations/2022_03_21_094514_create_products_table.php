79de510830670d08dd3c9e062cfc667a0efda04e
Andre <andrew.kanatow@gmail.com>
2022-03-21 15:31:42 +0200

    migrations

---
+++ marketplace/database/migrations/2022_03_21_094514_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('meta_title')->nullable();
+            $table->unsignedFloat('price');
+            $table->text('description');
+            $table->text('meta_description')->nullable();
+            $table->string('slug')->unique();
+            $table->enum('in_stock', ['available', 'coming soon', 'on order'])->default('available');
+            $table->boolean('newness')->default(1);
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
