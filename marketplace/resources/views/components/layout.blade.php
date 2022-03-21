aef8f3a903848581335fd10bb50013c797409014
Andre <andrew.kanatow@gmail.com>
2022-03-21 22:59:37 +0200

    home devnt

--- marketplace/resources/views/components/layout.blade.php
+++ marketplace/resources/views/components/layout.blade.php
@@ -5,25 +5,62 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 
+<div class="container">
+    <header class="container">
+        <nav class="navbar navbar-light bg-light justify-content-around row">
+            <a class="navbar-brand col-1" href="/">Home</a>
+            <div class="col-2" x-data="{ open: false }">
+                <div class="dropdown">
+                    <button @click="open=!open" class="btn btn-secondary dropdown-toggle" type="button"
+                            id="dropdownMenuButton">
+                        All categories
+                    </button>
+                    <div x-show="open" @click.outside="open = false" style="position: absolute;
+    z-index: 1000;
+    display: none;
+    min-width: 10rem;
+    padding: 0.5rem 0;
+    margin: 0;
+    font-size: 0.9rem;
+    color: #212529;
+    text-align: left;
+    list-style: none;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    border-radius: 0.25rem;">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </div>
+            </div>
+            <form class="form-inline col-4">
+                <input class="form-control mr-sm-2 w-75 d-inline-block" type="search" placeholder="Search"
+                       aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <div class="col-3 nav-item">
+                <a href="#" class="nav-link"></a>
+                <a href="#" class="nav-link"></a>
+                <a href="#" class="nav-link"></a>
+            </div>
 
-<header class="flex justify-between items-center">
-    <nav class="w-full bg-blue-300 h-16">
-        <div class="w-1/5 h-full"></div>
-    </nav>
-</header>
-<div>
-    {{$slot}}
+        </nav>
+    </header>
+    <main>
+        {{$slot}}
+    </main>
+    <footer class="bg-secondary mt-3" style="height: 100px">
+
+    </footer>
 </div>
-<footer class="">
-
-</footer>
-
 </body>
-<script src="//unpkg.com/alpinejs" defer></script>
+<script src="js/app.js"></script>
 </html>
 
--- marketplace/resources/views/home.blade.php
+++ marketplace/resources/views/home.blade.php
@@ -1,3 +1,46 @@
 <x-layout>
-    111
+    <section class="d-flex flex-column mt-3">
+        <a href="#" class="card bg-light text-black mt-3">
+            <img class="card-img" src="..." alt="Card image" style="height: 200px">
+            <div class="card-img-overlay">
+                <h2 class="card-title">Books</h2>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias amet cum
+                    debitis dignissimos, dolores ea eius excepturi id, impedit in inventore, itaque iusto magnam nam
+                    natus nesciunt obcaecati praesentium quae quaerat quia quo sint suscipit tenetur velit veniam. At
+                    consectetur consequuntur cum dicta dignissimos distinctio dolorem eligendi enim eos esse
+                    exercitationem harum, ipsum itaque magni minima mollitia nihil perferendis quis quisquam quo quos
+                    reprehenderit rerum, sequi similique vel vero voluptas! Accusamus alias aliquid assumenda atque,
+                    debitis dolore facilis, in incidunt itaque magni minus necessitatibus nemo nulla odio omnis tempora,
+                    tenetur vitae. Ab consequuntur ex, iure perspiciatis provident totam!</p>
+            </div>
+        </a>
+        <a href="#" class="card bg-light text-black mt-3">
+            <img class="card-img" src="..." alt="Card image" style="height: 270px">
+            <div class="card-img-overlay">
+                <h2 class="card-title">Bugs</h2>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias amet cum
+                    debitis dignissimos, dolores ea eius excepturi id, impedit in inventore, itaque iusto magnam nam
+                    natus nesciunt obcaecati praesentium quae quaerat quia quo sint suscipit tenetur velit veniam. At
+                    consectetur consequuntur cum dicta dignissimos distinctio dolorem eligendi enim eos esse
+                    exercitationem harum, ipsum itaque magni minima mollitia nihil perferendis quis quisquam quo quos
+                    reprehenderit rerum, sequi similique vel vero voluptas! Accusamus alias aliquid assumenda atque,
+                    debitis dolore facilis, in incidunt itaque magni minus necessitatibus nemo nulla odio omnis tempora,
+                    tenetur vitae. Ab consequuntur ex, iure perspiciatis provident totam!</p>
+            </div>
+        </a>
+        <a href="#" class="card bg-light text-black mt-3">
+            <img class="card-img" src="..." alt="Card image" style="height: 270px">
+            <div class="card-img-overlay">
+                <h2 class="card-title">Sport inventory</h2>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias amet cum
+                    debitis dignissimos, dolores ea eius excepturi id, impedit in inventore, itaque iusto magnam nam
+                    natus nesciunt obcaecati praesentium quae quaerat quia quo sint suscipit tenetur velit veniam. At
+                    consectetur consequuntur cum dicta dignissimos distinctio dolorem eligendi enim eos esse
+                    exercitationem harum, ipsum itaque magni minima mollitia nihil perferendis quis quisquam quo quos
+                    reprehenderit rerum, sequi similique vel vero voluptas! Accusamus alias aliquid assumenda atque,
+                    debitis dolore facilis, in incidunt itaque magni minus necessitatibus nemo nulla odio omnis tempora,
+                    tenetur vitae. Ab consequuntur ex, iure perspiciatis provident totam!</p>
+            </div>
+        </a>
+    </section>
 </x-layout>
