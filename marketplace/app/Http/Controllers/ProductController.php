7f9f23405b46f9252f5a055e2d3bf402247bf7d4
Andre <andrew.kanatow@gmail.com>
2022-03-28 12:11:55 +0300

    ssh

--- marketplace/app/Http/Controllers/ProductController.php
+++ marketplace/app/Http/Controllers/ProductController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(Category $category)
     {
-        return view('product.index', ['category' => $category]);
+        return view('product.index', ['products'=>Product::latest()
+            ->where([['category_id', $category->id], ['active', 1]])->paginate(3)->withQueryString()]);
     }
 }
--- marketplace/resources/views/product/index.blade.php
+++ marketplace/resources/views/product/index.blade.php
@@ -1,3 +1,25 @@
 <x-layout>
-    {{dd($category)}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <article class="col-3"></article>
+            <article class="col-9 d-flex flex-wrap justify-content-evenly start-0">
+                @if($products->count())
+                    @foreach($products as $product)
+                        <div class="card mb-3" style="width: 16rem;">
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$product->title}}</h5>
+                                <p class="fw-bold">{{$product->price}}$</p>
+                                <p class="card-text">{{$product->description}}</p>
+                                <a href="#" class="btn btn-primary">Add to Favorites</a>
+                            </div>
+                        </div>
+                    @endforeach
+                @else
+                    <p style="font-weight: bold; font-size: 20px; padding-top: 3rem">No products )=</p>
+                @endif
+            </article>
+            <nav class="d-flex justify-content-center pt-3">{{$products->links()}}</nav>
+        </div>
+    </div>
 </x-layout>
