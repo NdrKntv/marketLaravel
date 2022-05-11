d4bd34c395daa662b7ce897f5e99183adc28c6bf
Andre <andrew.kanatow@gmail.com>
2022-05-11 18:46:42 +0300

    need ajax for favs

--- marketplace/routes/breadcrumbs.php
+++ marketplace/routes/breadcrumbs.php
@@ -1,15 +1,23 @@
 <?php
 
 use DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs;
+use Illuminate\Support\Facades\URL;
 
 Breadcrumbs::for('home', function ($trail) {
     $trail->push('Home', route('home'));
 });
 Breadcrumbs::for('products', function ($trail, $category) {
     $trail->parent('home');
-    $trail->push($category->title, route('products', $category));
+    $trail->push(is_array($category) ? $category['category']->title : $category->title, route('products', $category));
 });
 Breadcrumbs::for('product', function ($trail, $category, $product) {
-    $trail->parent('products', $category);
+    $prevUrl = parse_url(URL::previous());
+    if (isset($prevUrl['path']) && $prevUrl['path'] == '/' . $category->slug . '/products' && isset($prevUrl['query'])) {
+        parse_str($prevUrl['query'], $params);
+        $params['category'] = $category;
+    } else {
+        $params = $category;
+    }
+    $trail->parent('products', $params);
     $trail->push($product->title, route('product', [$category, $product]));
 });
