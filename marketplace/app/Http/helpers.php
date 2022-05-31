5c16acb456b15ac84ddc8497c003b3ae73f4a315
Andre <andrew.kanatow@gmail.com>
2022-05-31 20:28:12 +0300

    -

--- marketplace/app/Http/helpers.php
+++ marketplace/app/Http/helpers.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Support\Str;
-
-if (!function_exists('slugGenerator')){
-    function slugGenerator($title, $model)
-    {
-        $slug = Str::slug($title);
-        $count = $model::whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")->withTrashed()->count();
-
-        return $count ? "$slug-$count" : $slug;
-    }
-}
+//use Illuminate\Support\Str;
+//
+//if (!function_exists('slugGenerator')){
+//    function slugGenerator($title, $model)
+//    {
+//        $slug = Str::slug($title);
+//        $count = $model::whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")->withTrashed()->count();
+//
+//        return $count ? "$slug-$count" : $slug;
+//    }
+//}
--- marketplace/app/Traits/SetSlug.php
+++ marketplace/app/Traits/SetSlug.php
@@ -2,12 +2,22 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Str;
+
 trait SetSlug
 {
+    private function slugGenerator($title, $model)
+    {
+        $slug = Str::slug($title);
+        $count = $model::whereRaw("slug RLIKE '^{$slug}(-[0-9]+)?$'")->withTrashed()->count();
+
+        return $count ? "$slug-$count" : $slug;
+    }
+
     protected static function boot()
     {
         parent::boot();
 
-        static::creating(fn($model) => $model->setAttribute('slug', slugGenerator($model->title, $model)));
+        static::creating(fn($model) => $model->setAttribute('slug', $this->slugGenerator($model->title, $model)));
     }
 }
